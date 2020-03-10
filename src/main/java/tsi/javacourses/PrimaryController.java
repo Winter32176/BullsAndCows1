577e051696fe3391078db8d3f4b9a1d118adcf03
JavaCourses1 <robert321766@outlook.com>
2020-03-10 19:33:52 +0200

    00

--- src/main/java/tsi/javacourses/PrimaryController.java
+++ src/main/java/tsi/javacourses/PrimaryController.java
@@ -1,35 +1,75 @@
 package tsi.javacourses;
 
 import java.io.IOException;
+import java.util.*;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
 
 public class PrimaryController {
 
     private int count;
+    private List<Integer> myNumbers;
     public Spinner<Integer> num1;
     public Spinner<Integer> num2;
     public Spinner<Integer> num3;
     public Spinner<Integer> num4;
     public TableView<Turn> turnsTable;
+    public Button goButton;
 
+    public void initialize() {
+        generateRandom();
+
+        goButton.disableProperty().bind(
+                Bindings.createBooleanBinding(
+                        () -> {
+                            Set<Integer> tmp = new HashSet<>();
+                            tmp.add(num1.getValue());
+                            tmp.add(num2.getValue());
+                            tmp.add(num3.getValue());
+                            tmp.add(num4.getValue());
+
+                            return tmp.size() < 4;
+                        }
+                        , num1.valueProperty()
+                        , num2.valueProperty()
+                        , num3.valueProperty()
+                        , num4.valueProperty()
+                )
+        );
+    }
+
+    private void generateRandom() {
+        Set<Integer> tmp = new LinkedHashSet<>();
+        Random rand = new Random();
+        while (tmp.size() < 4) {
+            int i = rand.nextInt(10);
+            tmp.add(i);
+        }
+        myNumbers = List.copyOf(tmp);
+
+        System.out.println(myNumbers);
+    }
 
     public void doTurn() {
-
         count++;
         int n1 = num1.getValue();
         int n2 = num2.getValue();
         int n3 = num3.getValue();
         int n4 = num4.getValue();
 
+        List<Integer> userNumber = List.of(n1,n2,n3,n4);
+
+
         var turn = new Turn();
         turn.setNr(count);
         turn.setGuess("" + n1 + n2 + n3 + n4);
 
-        turnsTable.getItems().add(0,turn);
+        turnsTable.getItems().add(0, turn);
 
         System.out.printf("%d %d %d %d %n", n1, n2, n3, n4);
 
